daf3bbca06ebb922c988c051de56a9a075f6b33d
MyangSZ <unoyou@naver.com>
2025-03-03 17:38:19 +0900

    1-9 Todo App 구조생성

---
+++ 1-9ReateNative_TODO_App/components/TodoItem.js
@@ -0,0 +1,6 @@
+# React Native
+
+- Todo App 만들기 
+
+- 1. 구조생성 (chapter1-9)
+- 2. Core COmponents 이용 UI작성하기 (cheapter 2-1)
---
+++ 1-9ReateNative_TODO_App/screens/MainScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  );
+};
+export default MainScreen;
+const styles = StyleSheet.create({});
